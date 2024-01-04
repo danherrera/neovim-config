e882498ba4f3e4b4b8e0080d1549987a1a4ccbc7
Danilo Herrera <danilo@agileninja.io>
2024-01-03 21:41:51 -0500

    Add none-ls formatting

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,26 @@
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.formatting.shfmt,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+				null_ls.builtins.formatting.rustfmt,
+				null_ls.builtins.formatting.clang_format,
+				null_ls.builtins.formatting.goimports,
+				null_ls.builtins.formatting.gofmt,
+				null_ls.builtins.formatting.sqlformat,
+				null_ls.builtins.formatting.sqlformat,
+				null_ls.builtins.formatting.terraform_fmt,
+				null_ls.builtins.formatting.terraform_fmt,
+				null_ls.builtins.formatting.terraform_fmt,
+				null_ls.builtins.formatting.terraform,
+			},
+		})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
+}
