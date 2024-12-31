aee07e23726479fed90bf8acd8ffc0671b911d18
Danilo Herrera <danilo@agileninja.io>
2024-12-31 16:29:55 -0500

    Update config name

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,28 +1,38 @@
 return {
-  "folke/trouble.nvim",
-  dependencies = {
-    "kyazdani42/nvim-web-devicons",
-  },
-  config = function()
-    require("trouble").setup {
-      auto_open = false,
-      auto_close = true,
-      auto_preview = false,
-      auto_fold = false,
-      signs = {
-        error = "error",
-        warning = "warning",
-        hint = "hint",
-        information = "info",
-        other = "other",
-      },
-      use_lsp_diagnostic_signs = true,
-    }
+	"folke/trouble.nvim",
+	dependencies = {
+		"kyazdani42/nvim-web-devicons",
+	},
+	config = function()
+		require("trouble").setup({
+			auto_open = false,
+			auto_close = true,
+			auto_preview = false,
+			auto_fold = false,
+			signs = {
+				error = "error",
+				warning = "warning",
+				hint = "hint",
+				information = "info",
+				other = "other",
+			},
+			use_diagnostic_signs = true,
+		})
 
-    vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-    vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-    vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
-    vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-    vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-  end,
+		vim.keymap.set("n", "<leader>xx", function()
+			require("trouble").toggle()
+		end)
+		vim.keymap.set("n", "<leader>xw", function()
+			require("trouble").toggle("workspace_diagnostics")
+		end)
+		vim.keymap.set("n", "<leader>xd", function()
+			require("trouble").toggle("document_diagnostics")
+		end)
+		vim.keymap.set("n", "<leader>xq", function()
+			require("trouble").toggle("quickfix")
+		end)
+		vim.keymap.set("n", "<leader>xl", function()
+			require("trouble").toggle("loclist")
+		end)
+	end,
 }
